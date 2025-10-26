15fda2e7a3029b30404b3b440cf0b6bf126b6d26
David Guerrero <dsgd2005@gmail.com>
2025-10-26 15:54:59 -0500

    Added navigation in between folders

--- create-project.sh
+++ create-project.sh
@@ -11,6 +11,28 @@ echo $'|                                          |'
 echo $'============================================'
 echo $'Shall we start?\n'
 
+echo $'First, choose a folder to create your project'
+echo
+sleep 1
+
+scripts_folder=$(pwd)
+
+while true; do
+    current_folder=$(pwd)
+
+    echo "You are on $current_folder right now"
+    read -e -p $'Would you like to: \n1 - Go back\n2 - Create and enter a folder\n3 - Enter to a folder (Make sure it exists)\n4 - Stay in this folder\n> ' menu
+    echo
+
+    case $menu in
+        1) cd .. ;;
+        2) read -p "Write the name of the foler: " folder; mkdir "$folder"; cd "$folder";;
+        3) ls; read -p "Write the name of the foler: " folder; cd "$folder";;
+        4) current_folder=$(pwd); break;;
+        *) echo "Error: Invalid option" ; exit 1 ;;
+    esac
+done
+
 # Ask for the type of project
 read -p $'What will your project include?\n1 - Backend and frontend\n2 - Only frontend\n> ' project
 echo
@@ -28,8 +50,10 @@ echo
 # Create the project
 if [ "$project" == "1" ]; then
     mkdir "$name-PROJECT"
-    ./create-backend-project.sh "$name-BACKEND"
-    ./create-frontend-project.sh "$name-FRONTEND"
+    cd "$scripts_folder"
+    ./create-backend-project.sh "$name-BACKEND" "$current_folder"
+    ./create-frontend-project.sh "$name-FRONTEND" "$current_folder"
 else
-    ./create-frontend-project.sh "$name"
+    cd "$scripts_folder"
+    ./create-frontend-project.sh "$name" "$current_folder"
 fi
